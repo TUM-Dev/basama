ed05dc470bb050e687f639f5cd25aaaa7da9f2a1
Frank Elsinga <frank@elsinga.de>
2023-04-27 23:21:24 +0200

    added a logo header

---
+++ web/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <NuxtLink to="/" id="header">
+      <VRow dense align-content="start" no-gutters>
+       <VImg src="../assets/logo.png" height="4rem" max-width="10rem" alt="The Logo of BaSaMa"></VImg>
+       <VImg src="../assets/associate.png" height="4rem" max-width="10rem" alt="The Logo of the offering University"></VImg>
+      </VRow>
+  </NuxtLink>
+</template>
+
+<style>
+#header{
+  position: absolute;
+  top: 1rem;
+  left: 1rem;
+  width: 100%;
+}
+</style>
+<script setup lang="ts">
+</script>
--- web/pages/index.vue
+++ web/pages/index.vue
@@ -2,27 +2,29 @@
   <VRow no-gutters align="center" justify="center" class="fill-width">
     <VImg
         src="/front.png"
-        cover
         class="h-100 d-flex align-center justify-content-start"
       >
       <VCol class="w-50 text-white me-auto ms-4" md="3" lg="4">
-          <h1 class="mb-4">Finding a thesis does not have to be an adventure</h1>
-          <VBtn>
-            <NuxtLink to="/login" class="font-weight-bold text-break">
-              Log in to find your thesis
-            </NuxtLink>
-          </VBtn>
-        </VCol>
-      </VImg>
+       <h1 class="mb-4">Finding a thesis does not have to be an adventure</h1>
+       <Header />
+        <VBtn>
+          <NuxtLink to="/login" class="font-weight-bold text-break">
+            Log in to find your thesis
+          </NuxtLink>
+        </VBtn>
+      </VCol>
+    </VImg>
   </VRow>
 
-    <VRow>
-  <VContainer>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores, inventore quia.
-            Dolorum dolores ad ipsum voluptatum rem, hic placeat, odio, odit numquam quod
-            veritatis accusantium assumenda! Sequi, provident in! Iure!
-      </p>
-  </VContainer>
-    </VRow>
+  <VRow>
+    <VContainer>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores, inventore quia.
+              Dolorum dolores ad ipsum voluptatum rem, hic placeat, odio, odit numquam quod
+              veritatis accusantium assumenda! Sequi, provident in! Iure!
+        </p>
+    </VContainer>
+  </VRow>
 </template>
+<script setup lang="ts">
+</script>
