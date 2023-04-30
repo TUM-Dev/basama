da3ef89c6d7d73ebbdc712954bd860cf384aee54
Frank Elsinga <frank@elsinga.de>
2023-04-30 14:05:57 +0200

    added the api documentation

---
+++ web/components/Api.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="swagger-ui"></div>
+</template>
+
+<script setup lang="ts">
+import "swagger-ui-dist/swagger-ui.css";
+import type { SwaggerUIBundle, Spec } from "swagger-ui-dist";
+
+window.setTimeout(() => {
+  // we need to make sure, that swagger-ui exists, otherwise the following command will fail
+  // therefore waiting is effective
+  import("swagger-ui-dist").then(({ SwaggerUIBundle }) =>
+    SwaggerUIBundle({
+      url: "/openapi.yaml",
+      dom_id: "#swagger-ui",
+      presets: [SwaggerUIBundle.presets.apis],
+    })
+  );
+}, 10);
+</script>
---
+++ web/pages/api.vue
@@ -0,0 +1,5 @@
+<template>
+  <ClientOnly>
+    <LazyApi />
+  </ClientOnly>
+</template>
