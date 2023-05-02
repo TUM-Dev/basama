20b2a38292d6a466761f8c11172ca57f8813ac79
Frank Elsinga <frank@elsinga.de>
2023-05-03 00:33:15 +0200

    removed unnessesary logging

--- web/pages/thesis/[id].vue
+++ web/pages/thesis/[id].vue
@@ -93,7 +93,6 @@ const thesis = ref({
 definePageMeta({
   validate: async (route) => {
     const UUID_REGEX = /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
-    console.log("got ", route.params.id, " => match: ", UUID_REGEX.test(route.params.id as string));
     return UUID_REGEX.test(route.params.id as string);
   }
 });
